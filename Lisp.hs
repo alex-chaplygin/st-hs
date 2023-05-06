293dd2ae50d484e910d223498b9aefd2f25e1fac
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-05-06 14:03:21 +0300

    Правильная интепретация результата

--- Lisp.hs
+++ Lisp.hs
@@ -4,15 +4,8 @@ import Data.List
 
 data Object = ATOM String
   | NUM Int
-  | NIL
-  | CONS Object Object
+  | LIST [Object]
   deriving (Show, Eq)
-
-data Result = INT Int
-  | OBJ Object
-  deriving Show
--- (1 2)
--- CONS (NUM 1) (CONS (NUM 2) NIL)
 
 isNotDigit c = c < '0' || c > '9'
 
@@ -24,37 +17,57 @@ number = do
 
 mychar c = do {skipSpaces; char c}
 
-expr :: ReadP Object
-atom = do {skipSpaces; c <- satisfy isNotDigit; s <- munch (\c -> c /= ' ' && c /= ')'); return $ ATOM (c:s)}
-obj = atom +++ number +++ expr
+atomFirst c = (isAlpha c || isSymbol c) && c /= '(' && c /= ')'
+atomSym c = (isAlphaNum c || isSymbol c) && c /= '(' && c /= ')'
 
-toCons :: [Object] -> Object
-toCons [] = NIL
-toCons (h:t) = CONS h (toCons t)
+qexpr :: ReadP Object
+atom = do {skipSpaces; c <- satisfy atomFirst; s <- munch atomSym; return $ ATOM $ map toUpper (c:s)}
+obj = atom +++ number +++ qexpr
 
-expr =  do {mychar '(' ; e <- many obj; mychar ')'; return $ toCons e}
-parse s = fst $ last $ readP_to_S expr s
+sexpr =  atom +++ do {mychar '(' ; e <- many obj; mychar ')'; return $ LIST e}
+qexpr = do {mychar '\''; e <- sexpr; return $ case e of
+               LIST l -> LIST $ (ATOM "QUOTE") : [LIST l]
+               b -> LIST $ (ATOM "QUOTE") : [b]
+           } +++ sexpr
+parse s = fst $ last $ readP_to_S qexpr s
 
-makeArgs :: Object -> [Int]
-eval :: Object -> Result
-eval (NUM i) = INT i
-eval (CONS car cdr) =
-  let (h:args) = makeArgs cdr in
+eq (ATOM a: ATOM b:_) = if a == b then ATOM "T" else LIST []
+eq (LIST a: LIST b:_) = if a == b then ATOM "T" else LIST []
+eq _ = LIST []
+
+evalCar (LIST l) = head l
+evalCar _ = error "not list in CAR"
+
+evalCdr (LIST l) = LIST $ tail l
+evalCdr _ = error "not list in CDR"
+
+--makeArgs :: [Object] -> [Int]
+eval :: Object -> Object
+eval (NUM i) = NUM i
+eval (LIST []) = LIST []
+eval (LIST (car:cdr)) =
+--  let (h:args) = makeArgs cdr in
   case car of
-   ATOM "+" -> INT $ foldl (+) h args
-   ATOM "-" -> INT $ foldl (-) h args
-   ATOM "*" -> INT $ foldl (*) h args
-   ATOM "/" -> INT $ foldl div h args
-   ATOM "QUOTE" -> let (CONS a b) = cdr in OBJ a
-   ATOM "EVAL" -> eval cdr
-   _ -> error "Not + or -"
+--   ATOM "+" -> INT $ foldl (+) h args
+--   ATOM "-" -> INT $ foldl (-) h args
+--   ATOM "*" -> INT $ foldl (*) h args
+--   ATOM "/" -> INT $ foldl div h args
+   ATOM "QUOTE" -> head cdr
+   ATOM "ATOM" -> case (eval $ head cdr) of
+                    LIST [] -> ATOM "T"
+                    ATOM _ -> ATOM "T"
+                    _ -> LIST []
+   ATOM "EQ" -> eq cdr
+   ATOM "CAR" -> evalCar $ eval $ head cdr
+   ATOM "CDR" -> evalCdr $ eval $ head cdr
+   _ -> error "Unknown function"
 eval s = error $ "Error: " ++ (show s)
 
-makeArgs NIL = []               
-makeArgs (CONS car cdr) =
-  let v = eval car in
-    case v of
-      INT i -> i : makeArgs cdr
-      _ -> error "Not Int"
+--makeArgs [] = []               
+--makeArgs (car:cdr) =
+--  let v = eval car in
+--    case v of
+--      INT i -> i : makeArgs cdr
+--      _ -> error "Not Int"
                
 
