7cd7333c5d1d9b746e8cf6a842553810e90af958
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-12-16 21:03:42 +0300

    Правильное цитирование, progn

--- Lisp2.hs
+++ Lisp2.hs
@@ -5,7 +5,7 @@ import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Control.Monad.State as S
 
-data Object = ATOM String
+data Object = SYMBOL String
   | NUM Int
   | LIST [Object]
   deriving (Eq)
@@ -17,8 +17,9 @@ data Result = OK Object
   deriving Eq
 
 instance Show Object where
-  show (ATOM s) = s
+  show (SYMBOL s) = s
   show (NUM i) = (show i)
+  show (LIST []) = "NIL"
   show (LIST l) = "(" ++ (concat $ intersperse " " $ map show l) ++ ")"
 
 isNotDigit c = c < '0' || c > '9'
@@ -36,23 +37,25 @@ atomFirst c = (isAlpha c || isMySymbol c) && c /= '(' && c /= ')'
 atomSym c = (isAlphaNum c || isMySymbol c) && c /= '(' && c /= ')'
 
 qexpr :: ReadP Object
-atom = do {skipSpaces; c <- satisfy atomFirst; s <- munch atomSym; return $ ATOM $ map toUpper (c:s)}
-obj = atom +++ number +++ qexpr
-
+symbol = do {skipSpaces; c <- satisfy atomFirst; s <- munch atomSym; return $ SYMBOL $ map toUpper (c:s)}
+atom = symbol +++ number
+obj = atom +++ qexpr
 sexpr =  atom +++ do {mychar '(' ; e <- many obj; mychar ')'; return $ LIST e}
 qexpr = do {mychar '\''; e <- sexpr; return $ case e of
-               LIST l -> LIST $ (ATOM "QUOTE") : [LIST l]
-               b -> LIST $ (ATOM "QUOTE") : [b]
+               LIST l -> LIST $ (SYMBOL "QUOTE") : [LIST l]
+               NUM n -> LIST $ (SYMBOL "QUOTE") : [NUM n]
+               b -> LIST $ (SYMBOL "QUOTE") : [b]
            } +++ sexpr
-parse s = readP_to_S qexpr s
+parse s = readP_to_S obj s
 
-evalAtom (LIST []) = ATOM "T"
-evalAtom (ATOM _) = ATOM "T"
-evalAtom _ = LIST []
+t = SYMBOL "T"
+nil = LIST []
+evalAtom (LIST []) = t
+evalAtom (SYMBOL _) = t
+evalAtom _ = nil
 
-eq (ATOM a: ATOM b:_) = if a == b then ATOM "T" else LIST []
-eq (LIST a: LIST b:_) = if a == b then ATOM "T" else LIST []
-eq _ = LIST []
+eq (SYMBOL a: SYMBOL b:_) = if a == b then t else nil
+eq _ = nil
 
 evalCar (LIST l) = head l
 evalCar _ = error "not list in CAR"
@@ -64,20 +67,21 @@ cons (a:[LIST b]) = LIST (a:b)
 cons _ = error "Invalid CONS"
 
 eval :: Environment -> Object -> (Object, Environment)
-cond _ [] = error "empty cond"
-cond env ((LIST (p:e)):t) = let (p', _) = eval env p in
-  if p' == ATOM "T" then eval env $ head e
-  else cond env t
 
-defun env (ATOM name:params:body) =
-  let lam = LIST $ (ATOM "LAMBDA"):params:body in
+cond _ [] = error "empty cond"
+cond env ((LIST (p:e)):tail) = let (p', _) = eval env p in
+  if p' == t then eval env $ head e
+  else cond env tail
+
+defun env (SYMBOL name:params:body) =
+  let lam = LIST $ (SYMBOL "LAMBDA"):params:body in
   Map.insert name lam env
 
 funcall env f args = case Map.lookup f env of
      Nothing -> error $ "Unknown function " ++ (show f)
      Just val -> eval env $ LIST $ val:args
 
-evalExpr f (h:t) = foldl f (unInt h) $ map unInt t
+evalExpr f (h:tail) = foldl f (unInt h) $ map unInt tail
   where unInt (NUM i) = i
         unInt _ = error "Не число в выражении"
 
@@ -87,35 +91,38 @@ makeEnv (LIST params) values = if length params /= length values then
   error "Invalid params count"
   else
   Map.fromList $ zip (map fromAtom params) values
-  where fromAtom (ATOM a) = a
+  where fromAtom (SYMBOL a) = a
         fromAtom _ = error "Not atoms in params"
 makeEnv _ _ = error "No params list"             
 
 eval e (NUM i) = (NUM i, e)
-eval env (ATOM var) = case Map.lookup var env of
+eval e (LIST []) = (LIST [], e)
+eval e (SYMBOL "T") = (t, e)
+eval e (SYMBOL "NIL") = (nil, e)
+eval env (SYMBOL var) = case Map.lookup var env of
      Nothing -> error $ "Unknown variable " ++ (show var)
      Just val -> (val, env)
-eval e (LIST []) = (LIST [], e)
-eval e (LIST (ATOM "QUOTE":cdr)) = (head cdr, e)
-eval env (LIST (ATOM "COND":cdr)) = cond env cdr
-eval env (LIST ((LIST (ATOM "LAMBDA":params:body)):args)) =
-  let args' = map fst $ map (eval env) args in -- вычислить аргументы
-  let env' = Map.union (makeEnv params args') env in -- вычислить новое окружение
-  eval env' $ head body
+eval e (LIST (SYMBOL "QUOTE":cdr)) = (head cdr, e)
+eval env (LIST (SYMBOL "COND":cdr)) = cond env cdr
+eval env (LIST (SYMBOL "PROGN":cdr)) = foldl (\(o, e) obj->eval e obj) (nil, env) cdr
+--eval env (LIST ((LIST (SYMBOL "LAMBDA":params:body)):args)) =
+--  let args' = map fst $ map (eval env) args in -- вычислить аргументы
+--  let env' = Map.union (makeEnv params args') env in -- вычислить новое окружение
+--  eval env' $ head body
 eval env (LIST (car:cdr)) =
   let args = map fst $ map (eval env) cdr in
   case car of
-   ATOM "+" -> (NUM $ evalExpr (+) args, env)
-   ATOM "-" -> (NUM $ evalExpr (-) args, env)
-   ATOM "*" -> (NUM $ evalExpr (*) args, env)
-   ATOM "/" -> (NUM $ evalExpr div args, env)
-   ATOM "ATOM" -> (evalAtom $ head args, env)
-   ATOM "EQ" -> (eq args, env)
-   ATOM "CAR" -> (evalCar $ head args, env)
-   ATOM "CDR" -> (evalCdr $ head args, env)
-   ATOM "CONS" -> (cons args, env)
-   ATOM "DEFUN" -> (ATOM "T", defun env cdr)
-   ATOM f -> funcall env f cdr
+   SYMBOL "+" -> (NUM $ evalExpr (+) args, env)
+   SYMBOL "-" -> (NUM $ evalExpr (-) args, env)
+   SYMBOL "*" -> (NUM $ evalExpr (*) args, env)
+   SYMBOL "/" -> (NUM $ evalExpr div args, env)
+   SYMBOL "SYMBOL" -> (evalAtom $ head args, env)
+   SYMBOL "EQ" -> (eq args, env)
+   SYMBOL "CAR" -> (evalCar $ head args, env)
+   SYMBOL "CDR" -> (evalCdr $ head args, env)
+   SYMBOL "CONS" -> (cons args, env)
+   SYMBOL "DEFUN" -> (SYMBOL "T", defun env cdr)
+   SYMBOL f -> funcall env f cdr
    _ -> error "Не функция"
 
 lookVar :: String -> S.State Environment Result
@@ -132,7 +139,7 @@ cond' ((LIST (p:e)):t) = do
   env <- S.get
   let (p', _) = S.runState (ev p) env
   case p' of
-    OK (ATOM "T") -> ev $ head e
+    OK (SYMBOL "T") -> ev $ head e
     OK _ -> cond' t
     ERROR s -> return $ ERROR s
 
@@ -142,11 +149,11 @@ cond' ((LIST (p:e)):t) = do
 --  eval env' $ head body
 
 ev (NUM i) = return $ OK $ NUM i
-ev (ATOM var) = lookVar var
+ev (SYMBOL var) = lookVar var
 ev (LIST []) = return $ OK $ LIST []
-ev (LIST (ATOM "QUOTE":cdr)) = return $ OK $ head cdr
-ev (LIST (ATOM "COND":cdr)) = cond' cdr
---ev (LIST ((LIST (ATOM "LAMBDA":params:body)):args)) = lambda' params body args
+ev (LIST (SYMBOL "QUOTE":cdr)) = return $ OK $ head cdr
+ev (LIST (SYMBOL "COND":cdr)) = cond' cdr
+--ev (LIST ((LIST (SYMBOL "LAMBDA":params:body)):args)) = lambda' params body args
 ev _ = return $ ERROR "ER"
 
 process :: S.StateT Environment IO ()
