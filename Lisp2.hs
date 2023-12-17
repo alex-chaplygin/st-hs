93ce9b07a43bfb7178bbce49b9ea95c886b252e9
Чаплыгин Александр <alex_chaplygin@mail.ru>
2023-12-17 10:20:29 +0300

    Упрощен вызов функций

--- Lisp2.hs
+++ Lisp2.hs
@@ -8,6 +8,7 @@ import qualified Control.Monad.State as S
 data Object = SYMBOL String
   | NUM Int
   | LIST [Object]
+  | LAMBDA Object [Object] -- функция параметры тело
   deriving (Eq)
 
 type Environment = Map String Object
@@ -21,9 +22,10 @@ instance Show Object where
   show (NUM i) = (show i)
   show (LIST []) = "NIL"
   show (LIST l) = "(" ++ (concat $ intersperse " " $ map show l) ++ ")"
+  show (LAMBDA args body) = "LAMBDA " ++ (show args) ++ " " ++ (concat $ intersperse " " $ map show body)
 
 isNotDigit c = c < '0' || c > '9'
-isMySymbol c = isSymbol c || c == '*' || c == '/'
+isMySymbol c = isSymbol c || c == '*' || c == '/' || c == '+' || c == '-'
 
 number :: ReadP Object
 number = do
@@ -41,21 +43,20 @@ symbol = do {skipSpaces; c <- satisfy atomFirst; s <- munch atomSym; return $ SY
 atom = symbol +++ number
 obj = atom +++ qexpr
 sexpr =  atom +++ do {mychar '(' ; e <- many obj; mychar ')'; return $ LIST e}
-qexpr = do {mychar '\''; e <- sexpr; return $ case e of
-               LIST l -> LIST $ (SYMBOL "QUOTE") : [LIST l]
-               NUM n -> LIST $ (SYMBOL "QUOTE") : [NUM n]
-               b -> LIST $ (SYMBOL "QUOTE") : [b]
+qexpr = do {mychar '\''; e <- sexpr; return $ LIST $ (SYMBOL "QUOTE") : [e]
            } +++ sexpr
 parse s = readP_to_S obj s
 
 t = SYMBOL "T"
 nil = LIST []
-evalAtom (LIST []) = t
-evalAtom (SYMBOL _) = t
-evalAtom _ = nil
+globalEnv = Map.fromList [("T", t), ("NIL", nil)]
 
+eq (LIST []: LIST []: []) = t
 eq (SYMBOL a: SYMBOL b:_) = if a == b then t else nil
 eq _ = nil
+
+equal (NUM a: NUM b: _) = if a == b then t else nil
+equal _ = nil
 
 evalCar (LIST l) = head l
 evalCar _ = error "not list in CAR"
@@ -74,12 +75,8 @@ cond env ((LIST (p:e)):tail) = let (p', _) = eval env p in
   else cond env tail
 
 defun env (SYMBOL name:params:body) =
-  let lam = LIST $ (SYMBOL "LAMBDA"):params:body in
+  let lam = LAMBDA params body in
   Map.insert name lam env
-
-funcall env f args = case Map.lookup f env of
-     Nothing -> error $ "Unknown function " ++ (show f)
-     Just val -> eval env $ LIST $ val:args
 
 evalExpr f (h:tail) = foldl f (unInt h) $ map unInt tail
   where unInt (NUM i) = i
@@ -95,66 +92,42 @@ makeEnv (LIST params) values = if length params /= length values then
         fromAtom _ = error "Not atoms in params"
 makeEnv _ _ = error "No params list"             
 
+-- применение функции
+--       Окружение -> Функция -> Аргументы -> (Результат, Новое окружение)
+apply :: Environment -> Object -> [Object] -> (Object, Environment)
+apply env (LAMBDA params body) args = 
+  let env' = Map.union (makeEnv params args) env in -- вычислить новое окружение
+    eval env' $ LIST (SYMBOL "PROGN":body) -- невный PROGN
+apply _ _ _ = error "Применяется не функция"
+
 eval e (NUM i) = (NUM i, e)
 eval e (LIST []) = (LIST [], e)
-eval e (SYMBOL "T") = (t, e)
-eval e (SYMBOL "NIL") = (nil, e)
 eval env (SYMBOL var) = case Map.lookup var env of
      Nothing -> error $ "Unknown variable " ++ (show var)
      Just val -> (val, env)
+-- особые формы     
 eval e (LIST (SYMBOL "QUOTE":cdr)) = (head cdr, e)
 eval env (LIST (SYMBOL "COND":cdr)) = cond env cdr
 eval env (LIST (SYMBOL "PROGN":cdr)) = foldl (\(o, e) obj->eval e obj) (nil, env) cdr
---eval env (LIST ((LIST (SYMBOL "LAMBDA":params:body)):args)) =
---  let args' = map fst $ map (eval env) args in -- вычислить аргументы
---  let env' = Map.union (makeEnv params args') env in -- вычислить новое окружение
---  eval env' $ head body
+eval env (LIST (SYMBOL "SETQ":SYMBOL var:expr)) =
+  let (v, _) = eval env (head expr) in (v, Map.insert var v env)
+eval env (LIST (SYMBOL "LAMBDA":args:body)) = (LAMBDA args body, env)
 eval env (LIST (car:cdr)) =
   let args = map fst $ map (eval env) cdr in
-  case car of
+  case car of -- встроенные примитивы
    SYMBOL "+" -> (NUM $ evalExpr (+) args, env)
    SYMBOL "-" -> (NUM $ evalExpr (-) args, env)
    SYMBOL "*" -> (NUM $ evalExpr (*) args, env)
    SYMBOL "/" -> (NUM $ evalExpr div args, env)
-   SYMBOL "SYMBOL" -> (evalAtom $ head args, env)
    SYMBOL "EQ" -> (eq args, env)
+   SYMBOL "EQUAL" -> (equal args, env)
    SYMBOL "CAR" -> (evalCar $ head args, env)
    SYMBOL "CDR" -> (evalCdr $ head args, env)
    SYMBOL "CONS" -> (cons args, env)
-   SYMBOL "DEFUN" -> (SYMBOL "T", defun env cdr)
-   SYMBOL f -> funcall env f cdr
-   _ -> error "Не функция"
-
-lookVar :: String -> S.State Environment Result
-lookVar var = do
-  env <- S.get
-  return $ case Map.lookup var env of
-     Nothing -> ERROR $ "Неизвестная переменная " ++ (show var)
-     Just val -> OK val
-
-ev :: Object -> S.State Environment Result
-
-cond' [] = return $ ERROR "Пустое условие COND"
-cond' ((LIST (p:e)):t) = do
-  env <- S.get
-  let (p', _) = S.runState (ev p) env
-  case p' of
-    OK (SYMBOL "T") -> ev $ head e
-    OK _ -> cond' t
-    ERROR s -> return $ ERROR s
-
---lambda' params body args =
---  let args' = map unOk $ map ev args in -- вычислить аргументы
---  let env' = Map.union (makeEnv params args') env in -- вычислить новое окружение
---  eval env' $ head body
-
-ev (NUM i) = return $ OK $ NUM i
-ev (SYMBOL var) = lookVar var
-ev (LIST []) = return $ OK $ LIST []
-ev (LIST (SYMBOL "QUOTE":cdr)) = return $ OK $ head cdr
-ev (LIST (SYMBOL "COND":cdr)) = cond' cdr
---ev (LIST ((LIST (SYMBOL "LAMBDA":params:body)):args)) = lambda' params body args
-ev _ = return $ ERROR "ER"
+   SYMBOL "LIST" -> (LIST args, env)
+   SYMBOL "DEFUN" -> (head cdr, defun env cdr)
+   f -> let (func, env') = eval env f in
+     apply env' func args
 
 process :: S.StateT Environment IO ()
 process = do
@@ -168,14 +141,12 @@ process = do
       S.liftIO $ putStrLn "Ошибка ввода"
       else do
       let (res, env) = eval e (fst $ last $ ob)
-      S.liftIO $ putStrLn (show res) --of
---        OK o -> (show o)
---        ERROR e -> "Ошибка: " ++ e
+      S.liftIO $ putStrLn (show res)
       S.put env
     process
 
 repl :: IO ()
 repl = do
-  let env = Map.empty
+  let env = globalEnv
   res <- S.runStateT process env
   return ()
