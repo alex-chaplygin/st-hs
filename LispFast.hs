1113680a0ea8d2fecc36770b10f46b7b591780ab
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-01-30 18:44:08 +0300

    Тест интерпретатора

--- LispFast.hs
+++ LispFast.hs
@@ -2,8 +2,7 @@
 import Text.ParserCombinators.ReadP
 import Data.Char
 import Data.List
-import Data.Vector (Vector, (!), (//), empty, cons)
-import Data.Hashable
+import qualified Data.Vector as V
 import qualified Control.Monad.State as S
 
 data Object = SYMBOL String
@@ -12,9 +11,9 @@ data Object = SYMBOL String
   | LAMBDA Object [Object] -- функция параметры тело окружение
   deriving (Eq)
 
-type Env = Vector Object -- массив значений (запись активации)
-type FrameList = [Env]
-type Cont = Object -> (Object, Env)-- продолжение
+type Env = [[String]] -- окружение - список кадров из переменных
+type FrameList = [V.Vector Object] -- записи активаций
+type Cont = Object -> S.StateT FrameList IO Object -- продолжение
 
 data Result = OK Object
   | ERROR String
@@ -58,14 +57,21 @@ parse s = readP_to_S obj s
 meaning :: Object -> Env -> FrameList -> Cont -> (Object, Env)
 meaning o@(NUM i) e = meaningQuote o e
 meaning (LIST (SYMBOL "QUOTE":o:[])) e = meaningQuote o e
--- meaning (SYMBOL name) e = meaningReference name e
---meaning (LIST (SYMBOL "LAMBDA":args:body)) e = meaningAbstraction args body e
+--meaning (SYMBOL name) e = meaningReference name e
+--meaning (LIST (SYMBOL "LAMBDA":(LIST args):body)) e = meaningAbstraction args body e
 meaning (LIST (SYMBOL "IF":expr:t:f:[])) e = meaningAlternative expr t f e
 meaning (LIST (SYMBOL "BEGIN":s)) e = meaningSequence s e
 --meaning (LIST (SYMBOL "SETQ":SYMBOL var:exp:[])) e = meaning-assignment var exp e
 --meaning (LIST (car:cdr)) e = meaningApplication car cdr e
 -- цитирование
 meaningQuote o e = \sr c -> c o
+-- чтение переменной
+--meaningReference name env = \sr c -> let (i, j) = localVar env name in case i of
+-- рассчет индексов локальных переменных для кадров активации
+localVar [] _ _ = Nothing
+localVar (car:cdr) i name = scan car 0 where
+  scan [] _ = localVar cdr (i + 1) name
+  scan (h:t) j = if name == h then Just (i, j) else scan t $ j + 1
 -- ветвление
 meaningAlternative e1 e2 e3 env = let m1 = meaning e1 env
                                       m2 = meaning e2 env
@@ -80,29 +86,35 @@ meaningSequence (o:t) e = let m1 = meaning o e
                               mt = meaningSequence t e in
                              \sr c -> m1 sr $ \v -> mt sr c
 -- применение функции
--- meaningApplication f args env = let m = meaning f env
---                                     vals = meaningArgs args env $ length args in
---                                   \sr c -> m sr $ \f' -> vals sr $ \v' -> f v' c
--- -- вычисление аргументов
--- meaningArgs [] _ _ = \sr c -> c empty
--- meaningArgs (car:cdr) env size = let m = meaning car env
---                                      mt = meaningArgs cdr env
---                                      idx = size - length cdr - 1 in
---                                    \sr c -> m sr $ \v -> mt sr $ \v' -> set-frame
--- -- абстракция
--- meaningAbstraction (LIST args) body env = let arity = length args
---                                               arity' = arity + 1
---                                               env' = cons args env
---                                               m = meaningSequence body env' in
---                                             \sr c -> c $ \o c' -> if length o == arity' then m (o : sr) c' else error "Неверное число аргументов"
+--meaningApplication f args env = let m = meaning f env
+--                                    vals = meaningArgs args env $ length args in
+--                                  \sr c -> m sr $ \f' -> case f' of
+--                                    LAMBDA (LIST args) body -> c f'
+--                                    _ -> error "Not function"
+-- вычисление аргументов
+--meaningArgs [] _ _ = \sr c -> c $ []
+--meaningArgs (car:cdr) env size = let m = meaning car env
+--                                     mt = meaningArgs cdr env size
+--                                     idx = size - length cdr - 1 in
+--                                   \sr c -> m sr $ \v -> mt sr $ \(v':t) ->
+--                                     c $ (v' V.// [(idx, v)]):t
+-- абстракция
+--meaningAbstraction :: [Object] -> [Object] -> Env -> FrameList -> Cont -> (Object, Env)
+--meaningAbstraction args body env = let arity = length args
+--                                       env' = [map (\a@(SYMBOL s)->s) args] ++ env
+--                                       m = meaningSequence body env' in
+--                                     \sr c -> c $ \o c' ->
+--                                       if length (V.fromList o) == arity then
+--                                         m (V.fromList o : sr) c'
+--                                       else error "Неверное число аргументов"
 
 -- глубокий поиск переменной - номер кадра - номер записи
 deepFetch :: FrameList -> Int -> Int -> Object
-deepFetch (car:cdr) 0 j = car ! j
+deepFetch (car:cdr) 0 j = car V.! j
 deepFetch (car:cdr) i j = deepFetch cdr (i - 1) j
 -- глубокое обновление переменной
 deepUpdate :: FrameList -> Int -> Int -> Object -> FrameList
-deepUpdate (car:cdr) 0 j v = (car // [(j, v)]) : cdr
+deepUpdate (car:cdr) 0 j v = (car V.// [(j, v)]) : cdr
 deepUpdate (car:cdr) i j v = deepUpdate cdr (i-1) j v
 
 process :: S.StateT Env IO ()
@@ -123,6 +135,6 @@ process = do
 
 repl :: IO ()
 repl = do
-  res <- S.runStateT process empty
+  res <- S.runStateT process []
   return ()
 main = repl
