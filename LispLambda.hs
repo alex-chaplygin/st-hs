4b6590266b4d6fae4c684c3b8cbd78cab048c1f5
Чаплыгин Александр <alex_chaplygin@mail.ru>
2024-02-22 14:36:04 +0300

    Правильные лексические замыкания

--- LispLambda.hs
+++ LispLambda.hs
@@ -9,7 +9,7 @@ import qualified Control.Monad.State as S
 data Object = SYMBOL String
   | NUM Int
   | LIST [Object]
-  | LAMBDA Object [Object] -- функция параметры тело окружение
+  | LAMBDA Object [Object] Env -- функция параметры тело окружение
   | CONT Cont -- объект - продолжение
 
 type Var = String -- переменная
@@ -23,7 +23,7 @@ instance Show Object where
   show (NUM i) = (show i)
   show (LIST []) = "NIL"
   show (LIST l) = "(" ++ (concat $ intersperse " " $ map show l) ++ ")"
-  show (LAMBDA args body) = "LAMBDA " ++ (show args) ++ " " ++ (concat $ intersperse " " $ map show body)
+  show (LAMBDA args body e) = "LAMBDA " ++ (show args) ++ " " ++ (concat $ intersperse " " $ map show body)
   show (CONT s) = "CONT"
 
 isNotDigit c = c < '0' || c > '9'
@@ -87,7 +87,7 @@ extendFMem :: Mem -> [Object] -> Mem
 extendFMem mem = let NUM n = mem 0 in fst . foldl (\(m, n) v -> (extendMem m (n + 1) v, n + 1)) (mem, n)
 -- применение функции
 applyFun :: Object -> [Object] -> Env -> Cont -> Mem -> (Object, Env, Mem)
-applyFun (LAMBDA (LIST params) body) args env c mem = (\vals e m -> evalBegin body (extendFEnv e m params) (\o _ _->c o e m) $ extendFMem m vals) (map (\x->(\(x, _, _) -> x) $ eval x env c mem) args) env mem
+applyFun (LAMBDA (LIST params) body fenv) args env c mem = (\vals e m -> evalBegin body (extendFEnv e m params) (\o _ _->c o env m) $ extendFMem m vals) (map (\x->(\(x, _, _) -> x) $ eval x env c mem) args) fenv mem
 applyFun (CONT cc) args env c mem = cc (head args) env mem
 
 eval (NUM i) e c m = c (NUM i) e m
@@ -99,8 +99,8 @@ eval (LIST (SYMBOL "BEGIN":cdr)) env cc mem = evalBegin cdr env cc mem
 eval (LIST (SYMBOL "IF":expr:t:f:[])) env cc mem = eval expr env
   (\e _ m -> (\p -> p (eval t env cc m) (eval f env cc m)) $ toBool e) mem
 eval (LIST (SYMBOL "SETQ":SYMBOL var:expr:[])) env cc mem = eval expr env (\v e m -> let e' = extendEnv env m var in cc v e' $ extendMem m (e' var) v) mem
-eval (LIST (SYMBOL "LAMBDA":args:body)) env c mem = c (LAMBDA args body) env mem
-eval (LIST (SYMBOL "CALL/CC":LIST (SYMBOL "LAMBDA":args:body):[])) env cc mem = applyFun (LAMBDA args body) [CONT cc] env cc mem
+eval (LIST (SYMBOL "LAMBDA":args:body)) env c mem = c (LAMBDA args body env) env mem
+eval (LIST (SYMBOL "CALL/CC":LIST (SYMBOL "LAMBDA":args:body):[])) env cc mem = applyFun (LAMBDA args body env) [CONT cc] env cc mem
 eval (LIST (SYMBOL "CONS":car:cdr:[])) e c m =
   let (car', _, _) = eval car e c m
       (LIST cdr', _, _) = eval cdr e c m in c (LIST (car':cdr')) e m
